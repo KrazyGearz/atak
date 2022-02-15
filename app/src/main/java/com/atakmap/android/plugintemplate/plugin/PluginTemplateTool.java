78716e68ed5784975accae9a339b46ed9e04bc07
Shawn Bisgrove <shawn_bisgrove@partech.com>
2022-02-15 13:11:19 -0500

    Revert "+ update to using the abstract tool class"
    This reverts commit 3f91177c7937ccfb65b3ff66b90ea803ef4ea39c.

--- app/src/main/java/com/atakmap/android/plugintemplate/plugin/PluginTemplateTool.java
+++ app/src/main/java/com/atakmap/android/plugintemplate/plugin/PluginTemplateTool.java
@@ -1,10 +1,28 @@
+
 package com.atakmap.android.plugintemplate.plugin;
 
-import android.content.Context;
-
 import com.atak.plugins.impl.AbstractPluginTool;
+import com.atakmap.android.ipc.AtakBroadcast;
 import com.atakmap.android.plugintemplate.PluginTemplateDropDownReceiver;
-public class PluginTemplateTool extends AbstractPluginTool {
+import com.atakmap.util.Disposable;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import transapps.mapi.MapView;
+import transapps.maps.plugin.tool.Group;
+import transapps.maps.plugin.tool.Tool;
+import transapps.maps.plugin.tool.ToolDescriptor;
+
+/**
+ * Please note:
+ *     Support for versions prior to 4.5.1 can make use of a copy of AbstractPluginTool shipped with
+ *     the plugin.
+ */
+public class PluginTemplateTool extends AbstractPluginTool implements Disposable {
 
     public PluginTemplateTool(Context context) {
         super(context,
@@ -12,5 +30,9 @@ public class PluginTemplateTool extends AbstractPluginTool {
                 context.getString(R.string.app_name),
                 context.getResources().getDrawable(R.drawable.ic_launcher),
                 PluginTemplateDropDownReceiver.SHOW_PLUGIN);
+    }
+
+    @Override
+    public void dispose() {
     }
 }
